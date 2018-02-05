767aa87522cae32724250cc65df26f693c71a002
Vividendi <nanostar78@gmail.com>
2018-02-05 10:38:17 +0100

    Initial commit

---
+++ Kub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning03_04
+{
+    class Kub
+    {
+        public double sida { get; set; }
+        public double Volym { get; set; }
+        public double Yta { get; set; }
+
+        public Kub(double sida)
+        {
+            this.sida = sida;
+        }
+        public void VolymBeräkning()
+        {
+            Volym = (Math.Pow(sida, 3));
+            Console.WriteLine("\tKubens Volym är {0}", Volym);
+        }
+        public void YteBeräkning()
+        {
+            Yta = 6 * (Math.Pow(sida, 2));
+            Console.WriteLine("\tKubens Yta är {0}", Yta);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning03_04
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Här ska användaren bestämma hur lång sidan ska vara
+            Console.WriteLine("\n\tAnge hur lång kubens sida är:\t\t");
+
+            double sida = Convert.ToDouble(Console.ReadLine()); /*Variabeldeklarering*/
+
+            //Här skapas det ett nytt kub-objekt där dess yta och volym beräknas genom en metod med hjälp av sidlängden
+            Kub kub1 = new Kub(sida);
+            kub1.VolymBeräkning();
+            kub1.YteBeräkning();
+
+            Console.ReadKey();
+
+        }
+    }
+}
